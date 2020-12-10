46807737fd4dc0e499783dde0ad32b2de18e059e
whb-bigdata <whb_bigdata@163.com>
2020-12-10 23:49:33 +0800

    day03scala两数之和

---
+++ src/main/scala/com/aibee/leetcode/DP01/SCNumsum02.scala
@@ -0,0 +1,16 @@
+package com.aibee.leetcode.DP01
+
+object SCNumsum02 {
+
+  def main(args: Array[String]): Unit = {
+    val arr = Array(2, 7, 11, 15)
+    print(sumnum(arr))
+  }
+
+  def sumnum(args: Array[Int]): Array[Int] = {
+    val arr = Array
+    for (elem <- args) {
+      if()
+    }
+  }
+}
