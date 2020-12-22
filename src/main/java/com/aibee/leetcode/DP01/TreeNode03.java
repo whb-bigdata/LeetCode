84179926bd61ef0507ec6bf71db755a40fd10464
whb-bigdata <whb_bigdata@163.com>
2020-12-22 23:58:36 +0800

    day06中序遍历

--- src/main/java/com/aibee/leetcode/DP01/TreeNode03.java
+++ src/main/java/com/aibee/leetcode/DP01/TreeNode03.java
@@ -1,7 +1,26 @@
 package com.aibee.leetcode.DP01;
+
+
+import scala.collection.immutable.List;
+import scala.collection.mutable.Stack;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
  *
  */
 public class TreeNode03 {
+    List<Integer> result = new LinkedList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root==null) return new LinkedList<>();
+
+        inorderTraversal(root.left);
+        result.add(root.val);
+        inorderTraversal(root.right);
+
+        return result;
+    }
 }
