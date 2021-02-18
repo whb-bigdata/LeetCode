56feb4839599c65e67bfc04470e1e60a78a3ee9a
whb-bigdata <whb_bigdata@163.com>
2021-02-18 23:11:48 +0800

    upload

--- src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
+++ src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
@@ -9,9 +9,6 @@ object MapUse {
     val immutableMap = Map(4 -> 8 , 5 -> 10)
     val map = immutableMap + (2 -> 4)
     var map1 = immutableMap + (2 -> 4)
-    var map3 = immutableMap + (2 -> 4)
-    var map4 = immutableMap + (2 -> 5)
-    print(map4)
     emptyMap("3") = 20
     println(emptyMap.getOrElse("3",0))
     val stringToInt: mutable.Map[String, Int] = scala.collection.mutable.Map[String, Int]()
