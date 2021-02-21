b4b77bc971f5a6abc6b4f753a935c85460050982
whb-bigdata <whb_bigdata@163.com>
2021-02-21 20:07:25 +0800

    upload

--- src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
+++ src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
@@ -8,6 +8,9 @@ object MapUse {
     val empyMapInt = new mutable.HashMap[Int, Int]()
     val immutableMap = Map(4 -> 8 , 5 -> 10)
     val map = immutableMap + (2 -> 4)
+    val t1 = new mutable.HashMap[Int,String]()
+    val map2 = t1 + (3 -> "333")
+    print(map2)
     emptyMap("3") = 20
     println(emptyMap.getOrElse("3",0))
 
