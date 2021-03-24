a2a9a3c00ab62162f0ff12b5a5b782ed5849f9ea
whb-bigdata <whb_bigdata@163.com>
2021-03-24 09:46:13 +0800

    sparksql

--- src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
+++ src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
@@ -13,7 +13,6 @@ object MapUse {
     print(map2)
     emptyMap("3") = 20
     emptyMap("3") = 20
-
     println(emptyMap.getOrElse("3",0))
     val stringToInt: mutable.Map[String, Int] = scala.collection.mutable.Map[String, Int]()
   }
