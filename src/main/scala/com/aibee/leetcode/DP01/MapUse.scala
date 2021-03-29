6beb1a931ed42b56a197f3f4f31c556f1e1c3a66
whb-bigdata <whb_bigdata@163.com>
2021-03-29 22:37:38 +0800

    sparksql

--- src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
+++ src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
@@ -11,6 +11,7 @@ object MapUse {
     val t1 = new mutable.HashMap[Int,String]()
     val map2 = t1 + (3 -> "333")
     println(map2)
+
     emptyMap("3") = 20
     emptyMap("3") = 20
     println(emptyMap.getOrElse("3",0))
