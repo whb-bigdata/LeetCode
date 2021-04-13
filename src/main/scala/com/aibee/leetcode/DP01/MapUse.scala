3f0b975da1e6720446b83c6730425332889b48e9
whb-bigdata <whb_bigdata@163.com>
2021-04-13 23:26:06 +0800

    sparksql

--- src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
+++ src/main/scala/com/aibee/leetcode/DP01/MapUse.scala
@@ -6,7 +6,20 @@ object MapUse {
   def main(args: Array[String]): Unit = {
     val emptyMap : mutable.HashMap[String,Int] = new mutable.HashMap[String,Int]()
     val empyMapInt = new mutable.HashMap[Int, Int]()
-    val immutableMap = Map(4 -> 8 , 5 -> 10)
+    val immutableMap:mutable.Map[Int,Int] = mutable.Map(4 -> 8 , 5 -> 10)
+    var z = Array("Runoob", "Baidu", "Google")
+    var im:mutable.Map[String,mutable.Map[String,Array[String]]] = mutable.Map("a" -> mutable.Map("a1" -> z))
+    var imtest:Array[String] = Array[String]()
+    val strings: Array[String] = im.get("a").get("a1")
+    var strings1: Array[String] = im.get("a").get("a")
+    val strings2: Array[String] = im.get("a").get("b")
+    val maybeStringToStrings: Option[mutable.Map[String, Array[String]]] = im.get("a")
+
+
+    val myMap: mutable.Map[String, String] = mutable.Map("key1" -> "value")
+    val value = ""
+    val maybeString = myMap.getOrElse("key1","a")
+    val value2: Option[String] = myMap.get("key2")
     val map = immutableMap + (2 -> 4)
     val t1 = new mutable.HashMap[Int,String]()
     val map2 = t1 + (3 -> "333")
